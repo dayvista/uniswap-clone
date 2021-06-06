dfb038055fe2c852d0a2b6fe0e9a0d71bbd92bc6
Liam Davis <liamdavis@tuta.io>
2021-06-06 16:17:20 -0600

    add proper inputValues initialState typing

--- src/lib/redux/features/inputValues.ts
+++ src/lib/redux/features/inputValues.ts
@@ -1,11 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type InputValuesState = {
+  fromVal: string;
+  toVal: string;
+};
+
+const initialState: InputValuesState = {
+  fromVal: null,
+  toVal: null,
+};
+
 export const inputValuesSlice = createSlice({
   name: "inputValues",
-  initialState: {
-    fromVal: null,
-    toVal: null,
-  },
+  initialState: initialState,
   reducers: {
     modifyFromValue: (state, action) => {
       state.fromVal = action.payload;
